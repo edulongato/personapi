19f2b3168af5f2d2c8a23318fc4bda3d93079837
Eduardo <edulongato@yahoo.com.br>
2021-09-01 14:30:28 -0300

    Emplementado a funcao delete.

--- src/main/java/com/edulongato/personapi/controller/PersonController.java
+++ src/main/java/com/edulongato/personapi/controller/PersonController.java
@@ -2,10 +2,13 @@ package com.edulongato.personapi.controller;
 
 import com.edulongato.personapi.dto.MessageResponseDTO;
 import com.edulongato.personapi.dto.request.PersonDTO;
+import com.edulongato.personapi.exception.PersonNotFoundException;
 import com.edulongato.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/people")
@@ -22,6 +25,22 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO){
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> ListAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 
 
