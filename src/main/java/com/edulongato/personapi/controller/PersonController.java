30afa9e33f1f842f239f0985fe338e4f4c7a45ba
Eduardo <edulongato@yahoo.com.br>
2021-09-01 11:03:46 -0300

    Criado pacote servivices e a classe personService

--- src/main/java/com/edulongato/personapi/controller/PersonController.java
+++ src/main/java/com/edulongato/personapi/controller/PersonController.java
@@ -2,7 +2,7 @@ package com.edulongato.personapi.controller;
 
 import com.edulongato.personapi.dto.MessageResponseDTO;
 import com.edulongato.personapi.entity.Person;
-import com.edulongato.personapi.repository.PersonRepository;
+import com.edulongato.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,19 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID" + savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
+
+
+
 }
